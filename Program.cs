2eaf5d9c82861d9ab0b407cb8e3c085704a22630
Stephen Madalo <stephenmadalo89@gmail.com>
2022-04-21 21:37:52 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SelStatements // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess what number I'm thinking of!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+
+            var userGuess = int.Parse(Console.ReadLine());
+
+
+            if (userGuess >= favNumber)
+            {
+                Console.WriteLine("Number guess is too high :P");
+            }
+            else if (userGuess <= favNumber)
+            {
+                Console.WriteLine("Number guess is too low :3 ");
+            }
+            else 
+            {
+                Console.WriteLine("Nevermind >:[");
+            }   
+        }
+    }
+}
